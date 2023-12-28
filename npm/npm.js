f807dee52c4e2979217f14f1e6fc6afd5576e1e8
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2023-12-28 19:13:17 +0300

    basic npm usage

---
+++ npm/npm.js
@@ -0,0 +1,7 @@
+// npm init for start
+// npm i one-liner-joke to install
+import joke from 'one-liner-joke'
+
+// basic usage for a package
+let randomJoke = joke.getRandomJoke().body
+console.log(randomJoke)
