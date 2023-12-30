cfd6d7c33367be287dcb4b1d0ee7b42e9f0580b7
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2023-12-30 03:21:16 +0300

    custom middleware added

--- middleware/index.js
+++ middleware/index.js
@@ -14,11 +14,29 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
 const port = 3000
 
+// custom middleWare
+function logger(req, res, next) {
+  console.log(`Method: ${req.method}`)
+  console.log(`Url: ${req.url}`)
+  next()
+}
+app.use(logger)
+
 // pre-processing part
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // logging part
 app.use(morgan('dev'))
+
+// just connect names
+let fullName = ''
+function connectName(req, res, next) {
+  let firstName = req.body.fName
+  let lastName = req.body.lName
+  fullName = `${firstName} ${lastName}`
+  next()
+}
+app.use(connectName)
 
 // send home page
 app.get('/', (req, res) => {
@@ -27,7 +45,7 @@ app.get('/', (req, res) => {
 
 // if user submit log the data
 app.post('/submit', (req, res) => {
-  console.log(req.body)
+  res.send(`<h1>Your Name is:</h1><h2>${fullName}</h2>`)
 })
 
 // log the listen part
