380afb91173bbbc3785c4d6b4dd52ce4face5449
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2024-01-13 03:07:57 +0300

    basic ejs code writed

---
+++ ejs/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+
+// define app and port
+const app = express()
+const port = 3000
+
+// send index.ejs and variables
+app.get('/', (req, res) => {
+  // take today number
+  const day = new Date()
+  const todayNumber = day.getDay()
+
+  //switch today number to day name
+  let today
+  switch (todayNumber) {
+    case 0:
+      today = 'sunday'
+      break
+    case 1:
+      today = 'monday'
+      break
+    case 2:
+      today = 'tuesday'
+      break
+    case 3:
+      today = 'wednesday'
+      break
+    case 4:
+      today = 'thursday'
+      break
+    case 5:
+      today = 'friday'
+      break
+    case 6:
+      today = 'saturday'
+      break
+  }
+
+  res.render('index.ejs', {
+    day: today,
+    advice: "Let's write some code.",
+  })
+})
+
+// log the listening port
+app.listen(port, () => {
+  console.log(`Server listening on ${port}.`)
+})
